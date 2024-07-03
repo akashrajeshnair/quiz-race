27e44ee915eaec8d60d46af397abfa1c6ca77f14
AdityaK238 <adikulkarni238@gmail.com>
2024-07-03 20:17:21 +0530

    first commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer: React.FC = () => {
+    return (
+        <footer className="footer">
+        <p>&copy; 2024 Quiz App. New Application.</p>
+        <ul>
+            <li>
+                <a href="https://x.com/?lang=en/quizapp">X</a>
+            </li>
+            <li>
+                <a href="https://www.facebook.com/quizapp">FaceBook</a>
+            </li>
+            <li>
+                <a href="https://www.instagram.com//quizapp">Instagram</a>
+            </li>
+        </ul>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,34 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-const inter = Inter({ subsets: ["latin"] });
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps>=({children})=> {
+  return (
+    <html>
+      <body>
+    <div className="container">
+      <head>
+        <title>Quiz App</title>
+        <meta name="description" content="Quiz App"/>
+      </head>
+      <Header/>
+      <main className="main">{children}</main>
+      <Footer/>
+    </div>
+    </body>
+    </html>
+  );
+};
+
+export default Layout;
+
+/*const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,3 +46,4 @@ export default function RootLayout({
     </html>
   );
 }
+*/
